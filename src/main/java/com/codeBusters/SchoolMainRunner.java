f60bc6bba5dfb08d7726bb3db3e4066150f3969e
Hrvoje <hbrkljacic@gmail.com>
2022-09-01 17:05:22 -0400

    D&H

--- src/main/java/com/codeBusters/SchoolMainRunner.java
+++ src/main/java/com/codeBusters/SchoolMainRunner.java
@@ -1,23 +1,21 @@
 package com.codeBusters;
 
+import com.codeBusters.entity.Course;
 import com.codeBusters.entity.Parent;
 import com.codeBusters.entity.School;
 import com.codeBusters.entity.Student;
-import com.codeBusters.service.CRUDService;
-import com.codeBusters.service.ParentService;
-import com.codeBusters.service.SchoolService;
-import com.codeBusters.service.StudentService;
+import com.codeBusters.service.*;
 
 public class SchoolMainRunner {
 
    public static CRUDService<School> schoolService;
-//    public static CRUDService<Course> courseService;
+   public static CRUDService<Course> courseService;
    public static CRUDService<Parent> parentService;
     public static CRUDService<Student> studentService;
-//
+
     static {
         SchoolMainRunner.schoolService = new SchoolService();
-//        SchoolMainRunner.courseService = new CourseService();
+       SchoolMainRunner.courseService = new CourseService();
        SchoolMainRunner.parentService = new ParentService();
        SchoolMainRunner.studentService = new StudentService();
     }
