953a2c18e0da6eb64d3dacce3e560b5d540cde27
Hrvoje <hbrkljacic@gmail.com>
2022-08-31 20:04:18 -0400

    D&H first commit

---
+++ src/main/java/com/codeBusters/service/StudentService.java
@@ -0,0 +1,33 @@
+package com.codeBusters.service;
+
+import com.codeBusters.entity.Student;
+
+import java.util.List;
+
+public class StudentService implements CRUDService<Student>{
+
+    @Override
+    public Student findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return null;
+    }
+
+    @Override
+    public void save(Student student) {
+
+    }
+
+    @Override
+    public void update(Student student) {
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+}
