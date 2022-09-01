34dd4222aa44a78de04b10fa0459c7356477e560
rasulshirinboev <rasulboev9@gmail.com>
2022-09-01 16:04:51 -0400

    first commit

---
+++ src/main/java/com/codeBusters/service/ParentService.java
@@ -0,0 +1,32 @@
+package com.codeBusters.service;
+
+import com.codeBusters.entity.Parent;
+
+import java.util.List;
+
+public class ParentService implements CRUDService<Parent> {
+    @Override
+    public Parent findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Parent> findAll() {
+        return null;
+    }
+
+    @Override
+    public void save(Parent parent) {
+
+    }
+
+    @Override
+    public void update(Parent parent) {
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+}
