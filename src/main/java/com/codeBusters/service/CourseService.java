1a61aefd368d6d055a9e2739a403b6cd670fa166
Umran Ozman <umonal@yahoo.com>
2022-09-06 16:44:43 -0400

    findById created

--- src/main/java/com/codeBusters/service/CourseService.java
+++ src/main/java/com/codeBusters/service/CourseService.java
@@ -1,15 +1,21 @@
 package com.codeBusters.service;
 
+import com.codeBusters.database.Database;
 import com.codeBusters.entity.Course;
+import com.codeBusters.entity.Student;
 
 import java.util.List;
 
 public class CourseService implements CRUDService<Course>{
 
     @Override
+
     public Course findById(int id) {
-        return null;
+        return Database.courseList.stream()
+                .filter(course -> course.getId() == id)
+                .findFirst().get();
     }
+
 
     @Override
     public List<Course> findAll() {
